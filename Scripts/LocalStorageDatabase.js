3106986fbab2bc0509ef59db4f2215beeaf6b881
Kingsley Tagbo <kingsleytagbo@gmail.com>
2018-12-12 15:46:23 -0600

    CRUD

---
+++ Scripts/LocalStorageDatabase.js
@@ -0,0 +1,105 @@
+/* Manages LocalStorage data operations */
+let database = new function () {
+
+    /* saves one item or a list of items */
+    this.save = function (key, data) {
+        let items = database.merge(key, data);
+        localStorage.setItem(key, JSON.stringify(items));
+    };
+
+    /* retrieves one item that matches a key */
+    this.fetch = function (key) {
+        let data = localStorage.getItem(key);
+        if (data) {
+            return database.sort(JSON.parse(data));
+        }
+        else {
+            database.removeAll(key);
+            return [];
+        }
+    };
+
+    this.sort = function (data) {
+        let scoreSorted = data.sort(function (first, second) {
+            if (first.score === second.score) {
+                return (second.lastName > first.lastName ? 1 : -1);
+            }
+            return (second.score - first.score);
+        });
+        return scoreSorted;
+    }
+
+    /* syncs both additions or modifications of data */
+    this.merge = function (key, data) {
+        let items = localStorage.getItem(key);
+        let dataItems = [];
+        if (items && data) {
+            dataItems = JSON.parse(items);
+            let item = data;
+            let found = false;
+            if ((!item.Id) || (item.Id.length === 0) || (item.Id === 0)) {
+                item.Id = database.getGuid();
+            }
+            if (dataItems.length > 0) {
+                for (let d = 0; d < dataItems.length; d++) {
+                    if (dataItems[d].Id === item.Id) {
+                        found = true;
+                        dataItems[d] = item;
+                        break;
+                    }
+                };
+            }
+            if (!found) {
+                dataItems.push(item);
+            }
+        }
+        return dataItems;
+    }
+
+    /* gets an empty model representing the data expected for this key */
+    this.getModel = function () {
+        let model = {
+            Id: '',
+            firstName: '',
+            lastName: '',
+            score: 0
+        }
+        return model;
+    }
+
+    /* removes one item that matches the key */
+    this.removeOne = function (key, data) {
+        let items = localStorage.getItem(key);
+        let match = false;
+        if (items && data) {
+            let dataItems = JSON.parse(items);
+            let i = dataItems.length
+            while (i--) {
+                if (data.Id === dataItems[i].Id) {
+
+                    dataItems.splice(i, 1);
+
+                    localStorage.setItem(key, JSON.stringify(dataItems));
+                    match = true;
+                    break;
+                }
+            }
+        }
+        return match;
+    }
+
+    /* removes all items for a key and resets to an empty array */
+    this.removeAll = function (key) {
+        localStorage.setItem(key, JSON.stringify([]));
+    }
+
+    /* generates a unique identifier similar to a Guid */
+    this.getGuid = function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+};
+
